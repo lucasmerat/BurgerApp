93b0b48baa02f017b1a615a26d40fe926a798038
Lucas Merat <lucas.merat@gmail.com>
2019-03-25 10:32:38 -0500

    Schema and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Double Double Animal Style');
+INSERT INTO burgers (burger_name) VALUES ('Mac and Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
